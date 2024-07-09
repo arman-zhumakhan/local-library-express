c320166dcef1b3de7366a653bbb4e67fffe31e94
Arman Zhumakhan <arman310501@gmail.com>
2024-07-09 11:44:36 +0800

    Add MongoDB and data models

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require("mongoose");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -21,6 +22,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+mongoose.set("strictQuery", false);
+const dev_db_url = "mongodb+srv://mongo:mongo@cluster0.xgodvg1.mongodb.net/local_library?retryWrites=true&w=majority&appName=Cluster0";
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
